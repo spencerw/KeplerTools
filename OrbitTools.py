1d6461da6ac12e71b1e0edb9d0835e6c5b268b28
Spencer Wallace <scw7@uw.edu>
2019-05-02 08:29:09 -0700

    Update functions and units

--- OrbitTools.py
+++ OrbitTools.py
@@ -1,8 +1,5 @@
 import MathHelpers
-from Units import *
 import numpy as np
-
-G = 6.67259e-8 # cm^3 g^-1 s^-2
 
 # Convert heliocentric coordinates and velocities to kepler orbital elements
 def cart2kep(pos, vel, m1, m2):
@@ -11,7 +8,6 @@ def cart2kep(pos, vel, m1, m2):
     
     r = np.array([X, Y, Z])
     v = np.array([vx, vy, vz])
-    G = 1
     mu = G*(m1+m2)
     magr = np.sqrt(X**2. + Y**2. + Z**2.)
     magv = np.sqrt(vx**2. + vy**2. + vz**2.)
@@ -56,8 +52,7 @@ def cart2kep(pos, vel, m1, m2):
 
 # Vectorized version
 def cart2kepX(X, Y, Z, vx, vy, vz, m1, m2):
-    G = 1
-    mu = G*(m1+m2)
+    mu = m1 + m2
     magr = np.sqrt(X**2. + Y**2. + Z**2.)
     magv = np.sqrt(vx**2. + vy**2. + vz**2.)
     
@@ -67,6 +62,7 @@ def cart2kepX(X, Y, Z, vx, vy, vz, m1, m2):
     evecx = tmpx/mu - X/magr
     evecy = tmpy/mu - Y/magr
     evecz = tmpz/mu - Z/magr
+
     e = np.sqrt(evecx**2. + evecy**2. + evecz**2.)
     
     a = MathHelpers.dot(hx, hy, hz, hx, hy, hz) / (mu * (1. - e**2.))
@@ -86,16 +82,20 @@ def cart2kepX(X, Y, Z, vx, vy, vz, m1, m2):
     omega[MathHelpers.dot(evecx, evecy, evecz, kvec[0], kvec[1], kvec[2]) < 0.] = 2.*np.pi - omega[MathHelpers.dot(evecx, evecy, evecz, kvec[0], kvec[1], kvec[2]) < 0.]
     
     theta = np.arccos(MathHelpers.dot(evecx, evecy, evecz, X, Y, Z) / (e * magr))
-    theta = np.where(MathHelpers.dot(X, Y, Z, vx, vy, vz) < 0., 2.*np.pi - theta, theta)
+    theta = np.where(MathHelpers.dot(X, Y, Z, vx, vy, vz) < 0., 2*np.pi - theta, theta)
                      
     E = np.arccos((e + np.cos(theta)) / (1 + e * np.cos(theta)))
-    E = np.where(np.logical_and(theta > np.pi, theta < 2.*np.pi), 2.*np.pi - E, theta)
+    E = np.where(np.logical_and(theta > np.pi, theta < 2*np.pi), 2*np.pi - E, theta)
     M = E - e*np.sin(E)
     
-    return a, e, inc, asc_node % (2.*np.pi), omega, M
+    return a, e, inc, asc_node % (2*np.pi), omega, M
 
 # Convert kepler orbital elements to heliocentric cartesian coordinates
 def kep2cart(a, ecc, inc, Omega, omega, M, mass, m_central):
+    a = a
+    mass = mass
+    m_central = m_central
+
     if inc == 0.:
         Omega = 0.
     if ecc == 0.:
@@ -104,8 +104,7 @@ def kep2cart(a, ecc, inc, Omega, omega, M, mass, m_central):
     
     X = a * (np.cos(E) - ecc)
     Y = a * np.sqrt(1. - ecc**2.) * np.sin(E)
-    G = 1.
-    mu = G * (m_central + mass)
+    mu = m_central + mass
     n = np.sqrt(mu / a**3.)
     Edot = n / (1. - ecc * np.cos(E))
     Vx = - a * np.sin(E) * Edot
@@ -131,27 +130,18 @@ def kep2cart(a, ecc, inc, Omega, omega, M, mass, m_central):
 # Get orbital parameters for particles in a pynbody snapshot
 # This assumes that the first particle is the central star
 def orb_params(snap):
-    x2 = snap.d['pos']
-    xpos = x2[1:][:,0] - x2[0][0]
-    ypos = x2[1:][:,1] - x2[0][1]
-    zpos = x2[1:][:,2] - x2[0][2]
-    v2 = snap.d['vel']
-    xvel = v2[1:][:,0] - v2[0][0]
-    yvel = v2[1:][:,1] - v2[0][1]
-    zvel = v2[1:][:,2] - v2[0][2]
+    x = snap.d['pos']
+    x_h = x[1:] - x[0]
+    v = snap.d['vel']
+    v_h = v[1:] - v[0]
     m1 = snap['mass'][0]
-    planetesimals = snap[1:]
-    m2 = planetesimals['mass']
+    pl = snap[1:]
+    m2 = pl['mass']
 
-    a, e, inc, asc_node, omega, M = cart2kepX(xpos, ypos, zpos, xvel, yvel, zvel, m1, m2)
-    planetesimals['a'] = a
-    planetesimals['e'] = e
-    planetesimals['inc'] = inc
-    planetesimals['asc_node'] = asc_node
-    planetesimals['omega'] = omega
-    planetesimals['M'] = M
+    pl['a'], pl['e'], pl['inc'], pl['asc_node'], pl['omega'], pl['M'] \
+        = cart2kepX(x_h[:,0], x_h[:,1], x_h[:,2], v_h[:,0], v_h[:,1], v_h[:,2], m1, m2)
     
-    return planetesimals
+    return pl
 
 # Convert kepler orbital elements to poincaire variables
 def kep2poinc(a, e, i, omega, Omega, M , m1, m2):
@@ -203,7 +193,7 @@ def lap(j, s, alpha):
     integral = integrate.quad(int_func, 0., 2.*np.pi)[0]
     return 1./np.pi*integral
 
-# First alpha derivative of disturbing function
+# First alpha derivative of laplace coefficient
 def d_lap(j, s, alpha):
     return s*(lap(j-1, s+1, alpha) - 2.*alpha*lap(j, s+1, alpha) + lap(j+1, s+1, alpha))
 
@@ -211,23 +201,43 @@ def d_lap(j, s, alpha):
 def f_d(j, alpha):
     return (j*lap(j, 0.5, alpha)) + (alpha/2.*d_lap(j, 0.5, alpha))
 
-# Libration width of first order resonance (Murray + Dermott 8.76)
-def res_width_fo(m, m_c, a, ecc, j2):
-    alpha = (j2/(j2+1))**(-2./3.)
-    alpha_f_d = alpha*f_d(j2, alpha)
+# Secular terms in disturbing function (from table 8.4 in M+D)
+def f_s1(alpha):
+    c1 = 1/4*alpha*d_lap(0, 1/2, alpha)
+    c2 = 1/16*alpha**2*d_lap(1, 3/2, alpha)
+    c3 = -1/8*alpha**3*d_lap(0, 3/2, alpha)
+    c4 = 1/16*alpha**2*d_lap(1, 3/2, alpha)
+    c5 = -1/8*alpha**2*lap(0, 3/2, alpha)
+    return c1 + c2 + c3 + c4 + c5
+
+def C_r(m_pert, m_c, a, j1, j2):
+    alpha = (j2/(j2-1))**(2/3)
+    P = 2*np.pi*np.sqrt(a**3/m_c)
+    n = 2*np.pi/P
+    return (m_pert/m_c)*n*alpha*f_d(j1, alpha)
+
+# Equation 13 from Winter and Murray 1998
+def curlypidot_d(m, m_c, a, e, j1, j2, j4, phi):
+    C_r_val = C_r(m, m_c, a, j1, j2)
+    return j4*C_r_val*e**(j4 - 2)*np.cos(phi)
+
+# Libration width of interior first order resonance (Murray + Dermott 8.76)
+def res_width_fo(m, m_c, a, ecc, j1, j2):
+    alpha = (j2/(j2-1))**(2./3.)
+    alpha_f_d = alpha*f_d(j1, alpha)
     Cr_n = np.fabs(m/m_c*alpha_f_d)
     da_a = np.sqrt(16./3.*Cr_n*ecc)*np.sqrt(1.+1./(27.*j2**2.*ecc**3)*Cr_n)-(2./(9.*j2*ecc)*Cr_n)
     return a*da_a
 
 # Libration frequency of first order resonance (Murray + Dermott 8.47)
-def res_lib_freq(m, m_c, a, ecc, j2):
-    alpha = (j2/(j2+1))**(-2./3.)
-    alpha_f_d = alpha*f_d(j2, alpha)
+def res_lib_freq(m, m_c, a, ecc, j1, j2):
+    alpha = (j2/(j2-1))**(2./3.)
+    alpha_f_d = alpha*f_d(j1, alpha)
     P = 2*np.pi*np.sqrt(a**3/m_c)
     n = 2*np.pi/P
     Crn = n**2*np.fabs(m/m_c*alpha_f_d)
     return np.sqrt(3*j2**2*Crn*ecc)
 
-# Isolation mass (Kokubo + Ida 2002)
+# Isolation mass in M_earth (Kokubo + Ida 2002)
 def m_iso(sigma, a, m, b):
-    return 0.16*(b/10)**(1/2)*(sigma/10)**(1/2)*a**2*m**(-1/2)*Mearth
+    return 0.16*(b/10)**(3/2)*(sigma/10)**(3/2)*a**3*m**(-1/2)
